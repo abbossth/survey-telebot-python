ad10402ca9c61e09891814e1b631f9f765a209d5
abbossth <bosskodevelopment@gmail.com>
2024-02-13 19:48:28 +0500

    project is nearly finished

--- data.py
+++ data.py
@@ -1,17 +1,402 @@
-import openpyxl
 
-wb = openpyxl.load_workbook("survey_db.xlsx")
+# Define the survey questions
+questions = [
+    {
+        "number": 1,
+        "question": "Тест 1",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 2,
+        "question": "Тест 2",
+        "has_sub_question": True,
+        "multiple_option_selection": False,
+        "is_sub_question": False,
+        "answers": []
+    },
+    {
+        "number": 21,
+        "question": "Тест 21",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": True
+    },
+    {
+        "number": 22,
+        "question": "Тест 22",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": True
+    },
+    {
+        "number": 23,
+        "question": "Тест 23",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": True
+    },
+    {
+        "number": 24,
+        "question": "Тест 24",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": True
+    },
+    {
+        "number": 3,
+        "question": "Тест 3",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 4,
+        "question": "Тест 4",
+        "answers": ["да", "нет"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 5,
+        "question": "Тест 5",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 6,
+        "question": "Тест 6",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 7,
+        "question": "Тест 7",
+        "answers": ["да", "нет"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 8,
+        "question": "Тест 8",
+        "answers": ["ипотечный кредит", "автокредит", "микрозайм"],
+        "has_sub_question": False,
+        "multiple_option_selection": True,
+        "is_sub_question": False
+    },
+    {
+        "number": 81,
+        "question": "Next",
+        "answers": ["Next"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 9,
+        "question": "Тест 9",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 10,
+        "question": "Тест 10",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 11,
+        "question": "Тест 11",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 12,
+        "question": "Тест 12",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 13,
+        "question": "Тест 13",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 14,
+        "question": "Тест 14",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 15,
+        "question": "Тест 15",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 16,
+        "question": "Тест 16",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 17,
+        "question": "Тест 17",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 18,
+        "question": "Тест 18",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 19,
+        "question": "Тест 19",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 20,
+        "question": "Тест 20",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+]
 
-records = wb["records"]
 
-
-def print_doc():
-    for row in records.values:
-        for value in row:
-            print(value)
-
-
-def add_record(data):
-    records.append(data)
-    print("Record appended successfully!")
+# Define the survey questions
+questions_uz = [
+    {
+        "number": 1,
+        "question": "Тест 1",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 2,
+        "question": "Тест 2",
+        "has_sub_question": True,
+        "multiple_option_selection": False,
+        "is_sub_question": False,
+        "answers": []
+    },
+    {
+        "number": 21,
+        "question": "Тест 21",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": True
+    },
+    {
+        "number": 22,
+        "question": "Тест 22",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": True
+    },
+    {
+        "number": 23,
+        "question": "Тест 23",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": True
+    },
+    {
+        "number": 24,
+        "question": "Тест 24",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": True
+    },
+    {
+        "number": 3,
+        "question": "Тест 3",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 4,
+        "question": "Тест 4",
+        "answers": ["да", "нет"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 5,
+        "question": "Тест 5",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 6,
+        "question": "Тест 6",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 7,
+        "question": "Тест 7",
+        "answers": ["да", "нет"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 8,
+        "question": "Тест 8",
+        "answers": ["ипотечный кредит", "автокредит", "микрозайм"],
+        "has_sub_question": False,
+        "multiple_option_selection": True,
+        "is_sub_question": False
+    },
+    {
+        "number": 9,
+        "question": "Тест 9",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 10,
+        "question": "Тест 10",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 11,
+        "question": "Тест 11",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 12,
+        "question": "Тест 12",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 13,
+        "question": "Тест 13",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 14,
+        "question": "Тест 14",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 15,
+        "question": "Тест 15",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 16,
+        "question": "Тест 16",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 17,
+        "question": "Тест 17",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 18,
+        "question": "Тест 18",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 19,
+        "question": "Тест 19",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+    {
+        "number": 20,
+        "question": "Тест 20",
+        "answers": ["1", "2", "3"],
+        "has_sub_question": False,
+        "multiple_option_selection": False,
+        "is_sub_question": False
+    },
+]
 
--- main.py
+++ main.py
@@ -1,106 +1,221 @@
 import telebot
 from telebot import types
-
 import config
+from data import questions, questions_uz
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token obtained from BotFather
 bot = telebot.TeleBot(config.BOT_TOKEN)
 
-questions = [
-    "Question 1: What is your favorite programming language?",
-    "Question 2: What is your favorite color?",
-    "Question 3: What is your favorite food?",
-    "Question 4: Where are you from?",
-    "Question 5: What is your favorite animal?",
-    "Question 6: Which programming languages do you know?"
-]
+# bot languages
+languages = [{"code": "uz", "text": "Узбек Тили"}, {"code": "ru", "text": "Русский Язык"}]
 
-answers = [
-    ["Python", "Java", "JavaScript", "C++", "Other"],
-    ["Red", "Blue", "Green", "Other"],
-    ["Pizza", "Sushi", "Burgers", "Other"],
-    ["North America", "Europe", "Asia", "Other"],
-    ["Dog", "Cat", "Bird", "Other"],
-    ["Python", "Java", "JavaScript", "C++", "Other"]
-]
+# chosen language || by default it is *ru*
+lang = "ru"
 
-pressed_buttons = {}
-current_question = 0
+# Keep track of user answers
+user_answers = {}
+
+# Keep track of question length
+user_question_length = {}
+
+# Keep track of the current question for each user
+user_current_question = {}
 
 
-@bot.message_handler(commands=['start', 'survey'])
-def send_first_question(message):
-    send_question(message, current_question)
+# Helper function to generate inline keyboard markup
+def generate_markup(question, answers):
+    select_symbol = "⚪️"
+    if int(question) == 11:
+        # select_symbol = "☐"
+        select_symbol = "◻️"
 
-
-def send_question(message, question_number):
-    if question_number < len(questions):
-        if question_number < 5:  # Radio buttons for questions 1-5
-            send_radio_question(message, questions[question_number], answers[question_number])
-        else:  # Checkbox for question 6
-            send_radio_question(message, questions[question_number], answers[question_number])
-    else:
-        bot.send_message(message.chat.id, "Thank you for completing the survey!")
-
-
-def send_radio_question(message, question, options):
     markup = types.InlineKeyboardMarkup()
-    for option in options:
-        markup.add(types.InlineKeyboardButton(text=f"⚪️ {option}", callback_data=option))
-    bot.send_message(message.chat.id, question, reply_markup=markup)
+    for answer in answers:
+        markup.add(types.InlineKeyboardButton(text=f"{select_symbol} {answer}", callback_data=f"{question}_{answer}_none"))
+    return markup
 
 
-def send_checkbox_question(message, question, options):
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    for option in options:
-        markup.add(types.KeyboardButton(text=f"⚪️ {option}"))
-    bot.send_message(message.chat.id, question, reply_markup=markup)
+# Helper function to generate inline keyboard markup
+def generate_markup_languages():
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    for answer in languages:
+        markup.add(types.InlineKeyboardButton(text=f"{answer["text"]}", callback_data=f"{answer["code"]}"))
+    return markup
 
 
+# finish
+def send_survey_finish_message(chat_id):
+    return bot.send_message(chat_id, f"Thank you very much!")
+
+
+# Handler for start command
+@bot.message_handler(commands=['start', 'survey'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Welcome to the Survey Bot! Let's get started with the survey.", reply_markup=generate_markup_languages())
+    user_current_question[message.chat.id] = 0  # Set the current question to the first question
+    user_answers[message.chat.id] = {}
+    user_question_length[message.chat.id] = len(questions)
+
+
+# Send survey question
+def send_question(chat_id):
+    print("*****************")
+    # print(user_answers[chat_id])
+    current_question = user_current_question[chat_id]
+
+    # Q1
+    q1_answer = user_answers[chat_id].get(0) if user_answers else None
+    q1_value = next(iter(q1_answer)) if q1_answer else None
+    if q1_value == "1" and current_question == 1:
+        q = 1
+        user_current_question[chat_id] += 7
+        if int(q) not in user_answers:
+            user_answers[chat_id][int(q)] = set()
+        user_answers[chat_id][q].add("-")
+
+    # Q4
+    q4_answer = user_answers[chat_id].get(7) if user_answers else None
+    q4_value = next(iter(q4_answer)) if q4_answer else None
+    if q4_value == "нет" and current_question == 8:
+        q = 8
+        user_current_question[chat_id] += 1
+        if int(q) not in user_answers:
+            user_answers[chat_id][int(q)] = set()
+        user_answers[chat_id][q].add("-")
+
+    # Q7
+    q7_answer = user_answers[chat_id].get(10) if user_answers else None
+    q7_value = next(iter(q7_answer)) if q7_answer else None
+    if q7_value == "нет" and current_question == 11:
+        return send_survey_finish_message(chat_id)
+
+    # Q-NEXT
+    # q_next_answer = user_answers[chat_id].get(12) if user_answers else None
+    # q_next_value = next(iter(q_next_answer)) if q_next_answer else None
+    # if q_next_value == "нет" and current_question == 11:
+    #     return send_survey_finish_message(chat_id)
+
+    if lang == "uz":
+        if current_question < user_question_length[chat_id]:
+            question = questions_uz[current_question]
+            return bot.send_message(chat_id, f"Question {question['number']}: {question['question']}", reply_markup=generate_markup(current_question, question["answers"]))
+        else:
+            send_survey_finish_message(chat_id)
+    elif lang == "ru":
+        if current_question < user_question_length[chat_id]:
+            question = questions[current_question]
+            # print(question)
+            if question["has_sub_question"]:
+                bot.send_message(chat_id, f"{question['question']}")
+                user_current_question[chat_id] += 1
+                current_question = user_current_question[chat_id]
+                question = questions[current_question]
+                return bot.send_message(chat_id, f"Question {question['number']}: {question['question']}",
+                                 reply_markup=generate_markup(current_question, question["answers"]))
+            else:
+                current_question = user_current_question[chat_id]
+                question = questions[current_question]
+                return bot.send_message(chat_id, f"Question {question['number']}: {question['question']}", reply_markup=generate_markup(current_question, question["answers"]))
+        else:
+            return send_survey_finish_message(chat_id)
+
+
+# Handler for inline keyboard button clicks
 @bot.callback_query_handler(func=lambda call: True)
-def handle_radio_answer(call):
-    text = pressed_buttons.get(call.message.text) if pressed_buttons else None
-    if call.data == text:
-        bot.send_message(call.message.chat.id, "Same Option")
-        return print("Same")
+def handle_callback_query(call):
+    global lang
+    curr = user_current_question.get(call.message.chat.id) if user_current_question else None
+    if curr != 0 and curr is not None:
         pass
     else:
-        selected_option = call.data
-        question_text = call.message.text
-        pressed_buttons[question_text] = selected_option
-        print(selected_option, question_text, pressed_buttons)
-        # Change the symbol from ☐ to ☑ (checked) when selected
-        markup = types.InlineKeyboardMarkup()
-        for option in answers[questions.index(question_text)]:
-            if option == selected_option:
-                markup.add(types.InlineKeyboardButton(text=f"🔘 {option}", callback_data=option))
-            else:
-                markup.add(types.InlineKeyboardButton(text=f"⚪️ {option}", callback_data=option))
+        user_current_question[call.message.chat.id] = 0  # Set the current question to the first question
+    # print(curr)
+    # print(user_current_question[call.message.chat.id])
+    # print("s: ", call.data)
+    if call.data == "ru":
+        lang = 'ru'
+        return send_question(call.message.chat.id)
+    if call.data == "uz":
+        lang = 'uz'
+        return send_question(call.message.chat.id)
+    if call.data == "12_next":
+        return send_question(call.message.chat.id)
 
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=question_text,
-                          reply_markup=markup)
-        send_next_question()
-        pass
-
-@bot.message_handler(func=lambda message: True)
-def handle_answer(message):
-    send_next_question(message)
-
-
-# def send_next_question(message):
-#     for i, question in enumerate(questions):
-#         if question not in pressed_buttons:
-#             send_question(message, i)
-#             return
-#     bot.send_message(message.chat.id, "Thank you for completing the survey!")
-
-
-def send_next_question(message):
-    for i, question in enumerate(questions):
-        if question not in pressed_buttons:
-            send_question(message, current_question+1)
+    question_idx, answer, select = call.data.split('_')
+    chat_id = call.message.chat.id
+    print("select: ", select)
+    if select == "selected":
+        if int(question_idx) == 11:
+            user_answers[chat_id][int(question_idx)].remove(answer)
+            print("Clicked checked option!")
+            print(user_answers[chat_id])
+        else:
             return
-    bot.send_message(message.chat.id, "Thank you for completing the survey!")
+    # Update user_answers dictionary
+    if select != "selected" and int(question_idx) not in user_answers[chat_id]:
+        user_answers[chat_id][int(question_idx)] = set()
+    answers = user_answers[chat_id][int(question_idx)]
+    print(user_current_question[chat_id])
+
+    if user_current_question[chat_id] != 12:  # this should be 11 after putting next button
+        answers.clear()
+
+    if select != "selected":
+        answers.add(answer)
+
+    selected_symbol = "🔘"
+
+    if int(question_idx) == 11:
+        # selected_symbol = "☑"
+        selected_symbol = "☑️"
+
+    print(user_answers[chat_id])
+    print("answer: ", answer)
+    print("user answer: ", user_answers[chat_id][int(question_idx)])
+    if answer != user_answers.get(question_idx):
+        # Change the button state from ⚪️ to 🔘
+        if lang == "ru":
+            # print("q: ", question_idx)
+            markup = generate_markup(question_idx, questions[int(question_idx)]["answers"])
+            # print("Markup: ", markup)
+            for row in markup.keyboard:
+                # print("row: ", row)
+                for button in row:
+                    btn_idx, a, select = button.callback_data.split('_')
+                    print("btn: ", button.callback_data)
+                    print("ua: ", user_answers[chat_id][int(question_idx)])
+                    if a in user_answers[chat_id][int(question_idx)]:
+                        button.text = f"{selected_symbol} {a}"
+                        button.callback_data = f"{btn_idx}_{a}_selected"
+                    # if button.callback_data == call.data:
+                    #     button.text = f"{selected_symbol} {answer}"
+                    #     button.callback_data = "selected"
+            bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=markup)
+        elif lang == "uz":
+            markup = generate_markup(question_idx, questions_uz[int(question_idx)]["answers"])
+            for row in markup.keyboard:
+                for button in row:
+                    if button.callback_data == call.data:
+                        button.text = f"{selected_symbol} {answer}"
+                        button.callback_data = f"{question_idx}_selected"
+            bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=markup)
+    idx = int(question_idx) if question_idx else None
+    curr_question = int(user_current_question.get(chat_id)) if user_current_question else None
+    # Update the current question for the user
+    if idx == curr_question:
+        # print(True)
+        if idx == 10:
+            user_current_question[chat_id] += 1
+            # Send next question if available, else end survey
+            send_question(chat_id)
+            user_current_question[chat_id] += 1
+            send_question(chat_id)
+        else:
+            user_current_question[chat_id] += 1
+            # Send next question if available, else end survey
+            send_question(chat_id)
+    return
 
 
+# Start the bot
 bot.polling()
